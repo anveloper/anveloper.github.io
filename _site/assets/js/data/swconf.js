744bb7360696e586cc20aef9f0b28cbc0b8c67cc
anveloper <hitedin@gmail.com>
2024-04-14 00:37:33 +0900

    chore: 지킬 생성 yml 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1713021623',resources: [
+    cacheName: 'chirpy-1713022438',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
