f0b590033ed76456269e6e0c4c3461b068b9e3ec
anveloper <hitedin@gmail.com>
2024-07-23 21:45:33 +0900

    chore: Next.js 블로그 제작 시작

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "https://anveloper.github.io" : "",
+};
+
+export default nextConfig;
