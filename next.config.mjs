c819fe3deeb4dd7d9da9bc9dea57ff03b5480dd0
anveloper <hitedin@gmail.com>
2024-07-23 22:32:00 +0900

    chore: reset file

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "export",
   assetPrefix:
-    process.env.NODE_ENV === "production" ? "https://anveloper.github.io" : "",
+    process.env.NODE_ENV === "production" ? "https://anveloper.github.io/" : "",
 };
 
 export default nextConfig;
