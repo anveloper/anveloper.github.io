61bb69ddcc052ad3ea58cf7957231cef0fd9ff36
anveloper <hitedin@gmail.com>
2026-02-01 17:10:50 +0900

    fix: framer-motion에서 motion/react로 import 경로 수정

--- components/animation/framer-wrapper.tsx
+++ components/animation/framer-wrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion } from "motion/react";
 
 type FramerMotionProps = {
   children: React.ReactNode;
