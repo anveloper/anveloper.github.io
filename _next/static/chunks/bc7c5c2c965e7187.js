528cbb3d179dd693570fef4c0cef529c83f36596
anveloper <anveloper@users.noreply.github.com>
2026-02-22 03:04:05 +0000

    deploy: 02eed0cd93ff3b1fa465a4b50b5eeca1ee0ae4a5

---
+++ _next/static/chunks/bc7c5c2c965e7187.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,318940,e=>{"use strict";var c=e.i(850654);e.i(599950),e.s([],655517),e.i(655517),e.s(["ArchitectureModule",()=>c.ArchitectureModule,"createArchitectureServices",()=>c.createArchitectureServices],318940)}]);
